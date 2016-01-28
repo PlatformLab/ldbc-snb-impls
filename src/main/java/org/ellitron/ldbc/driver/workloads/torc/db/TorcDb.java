6352b90a96b647077a18f385b664dbf904cccb46
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-01-27 16:17:17 -0800

    Added handler registrations.

--- src/main/java/org/ellitron/ldbc/driver/workloads/torc/db/TorcDb.java
+++ src/main/java/org/ellitron/ldbc/driver/workloads/torc/db/TorcDb.java
@@ -92,6 +92,13 @@ public class TorcDb extends Db {
     protected void onInit(Map<String, String> properties, LoggingService loggingService) throws DbException {
         registerOperationHandler(LdbcShortQuery1PersonProfile.class, LdbcShortQuery1PersonProfileHandler.class);
         registerOperationHandler(LdbcUpdate1AddPerson.class, LdbcUpdate1AddPersonHandler.class);
+        registerOperationHandler(LdbcUpdate2AddPostLike.class, LdbcUpdate2AddPostLikeHandler.class);
+        registerOperationHandler(LdbcUpdate3AddCommentLike.class, LdbcUpdate3AddCommentLikeHandler.class);
+        registerOperationHandler(LdbcUpdate4AddForum.class, LdbcUpdate4AddForumHandler.class);
+        registerOperationHandler(LdbcUpdate5AddForumMembership.class, LdbcUpdate5AddForumMembershipHandler.class);
+        registerOperationHandler(LdbcUpdate6AddPost.class, LdbcUpdate6AddPostHandler.class);
+        registerOperationHandler(LdbcUpdate7AddComment.class, LdbcUpdate7AddCommentHandler.class);
+        registerOperationHandler(LdbcUpdate8AddFriendship.class, LdbcUpdate8AddFriendshipHandler.class);
         
         connectionState = new BasicDbConnectionState(properties);
     }
