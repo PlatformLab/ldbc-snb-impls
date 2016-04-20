f64281ef60273b4621fb4578ca91532e5b7cf86e
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-04-19 22:15:45 -0700

    Added template code for Neo4jTransactionClient.
    This class provides an interface for executing queries against a
    transactional Cypher HTTP endpoint for Neo4j client applications.

---
+++ snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionClient.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ellitron.ldbcsnbimpls.interactive.neo4j;
+
+/**
+ * A Neo4j client class that provides an interface for executing Cypher queries
+ * against a transactional Cypher HTTP Endpoint. 
+ *
+ * @author Jonathan Ellithorpe (jde@cs.stanford.edu)
+ */
+public class Neo4jTransactionClient {
+
+  public Neo4jTransactionClient(String host, String port) {
+
+  }
+
+  public void stage(String query, String params) {
+
+  }
+
+  public String exec() {
+    return null;
+  }
+
+  public String commit() {
+    return null;
+  }
+
+  public void rollback() {
+
+  }
+}
