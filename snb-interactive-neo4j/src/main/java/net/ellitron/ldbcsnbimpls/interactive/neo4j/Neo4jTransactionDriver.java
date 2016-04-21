20974319c130e4fffdc57e475c079ace26a8a4b3
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-04-21 12:51:19 -0700

    Minor edits to comments for Neo4jTransactionDriver

--- snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionDriver.java
+++ snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionDriver.java
@@ -7,14 +7,17 @@ package net.ellitron.ldbcsnbimpls.interactive.neo4j;
 
 /**
  * A class for driving the execution of transactional Cypher statements against
- * a remote Neo4j server over the Neo4j REST API. In addition to supporting
- * typical interactive style transactions where the application executes
- * statements and views results in a loop before ultimately deciding to commit
- * or rollback, the driver also supports "blind" transactions where the
- * application composes one or more statements and both executes and commits in
- * one request to the server. In certain situations (for instance, adding a
- * node to the graph) this can save a round trip to the database and decrease
- * overall latency.
+ * a remote Neo4j server over the Neo4j REST API. An application uses the
+ * driver by queuing a sequence of statements in the driver, then calling
+ * methods to execute those statements in different ways.
+ * <p>
+ * In addition to supporting typical interactive style transactions where the
+ * application executes statements and views results in a loop before
+ * ultimately deciding to commit or rollback, the driver also supports "blind"
+ * transactions where the application composes one or more statements and both
+ * executes and commits in one request to the server. In certain simple
+ * situations (for instance, adding a node to the graph) this can save a round
+ * trip to the database and save latency.
  * <p>
  *
  * Failure Scenarios:<br>
@@ -69,11 +72,10 @@ public class Neo4jTransactionDriver {
   /**
    * Add one or more statements to the driver statement queue for future
    * execution. When executed, statements are always executed in FIFO order. A
-   * call to exec, commit, or rollback will generally have the effect of
-   * clearing the statement queue unless otherwise noted in special
-   * circumstances.
+   * call to exec, execAndCommit, or rollback will generally have the effect of
+   * clearing the statement queue unless otherwise noted.
    *
-   * @param stmts Neo4jStatement(s) to stage for later execution.
+   * @param stmts Neo4jStatement(s) to enqueue for later execution.
    */
   public void enqueue(Neo4jStatement... stmts) {
 
@@ -92,18 +94,18 @@ public class Neo4jTransactionDriver {
    * deciding whether to commit the transaction or to roll it back. Such use
    * looks like the following:<br>
    * <code>
-   * //... do work
-   * driver.enqueue(s1)
-   * //... do work
-   * driver.enqueue(s2, s3)
-   * results = driver.exec()
-   * // analyze results {s1: results[0], s2: results[1], s3: results[2]}
-   * // generate new statements
-   * driver.enqueue(s4, s5, s6)
-   * results = driver.exec()
-   * // analyze results
-   * // decide to commit (or rollback)
-   * driver.commit(); // or driver.rollback()
+   * //... do work<br>
+   * driver.enqueue(s1)<br>
+   * //... do work<br>
+   * driver.enqueue(s2, s3)<br>
+   * results = driver.exec()<br>
+   * // analyze results {s1: results[0], s2: results[1], s3: results[2]}<br>
+   * // generate new statements<br>
+   * driver.enqueue(s4, s5, s6)<br>
+   * results = driver.exec()<br>
+   * // analyze results<br>
+   * // decide to commit (or rollback)<br>
+   * driver.commit(); // or driver.rollback()<br>
    * </code> <br>
    * When interactive use is not needed, {@link #execAndCommit()} can be used
    * to execute statements and commit the transaction in one method call.
@@ -113,7 +115,7 @@ public class Neo4jTransactionDriver {
    * statement).
    */
   public Neo4jStatementResult[] exec() {
-
+    return null;
   }
 
   /**
@@ -126,14 +128,14 @@ public class Neo4jTransactionDriver {
    * Usage Notes:<br>
    * This method is particularly convenient and efficient for situations where
    * the decision to commit is not dependent on statement execution results
-   * (for instance, adding a node to the graph). A typical use case might look
-   * something like this:<br>
+   * (for instance, adding a node or set of nodes to the graph). A typical use
+   * case might look something like this:<br>
    * <code>
-   * while ((line = employees.readLine()) != null) {
-   *   // make cypher CREATE statement for employee
-   *   driver.enqueue(stmt);
-   * }
-   * driver.execAndCommit();
+   * while ((line = employees.readLine()) != null) {<br>
+   * &nbsp;&nbsp;// make cypher CREATE statement for employee<br>
+   * &nbsp;&nbsp;driver.enqueue(stmt);<br>
+   * }<br>
+   * driver.execAndCommit();<br>
    * </code>
    *
    * @return Array of results, where the ordering matches the statement enqueue
@@ -141,7 +143,7 @@ public class Neo4jTransactionDriver {
    * statement).
    */
   public Neo4jStatementResult[] execAndCommit() {
-
+    return null;
   }
 
   /**
