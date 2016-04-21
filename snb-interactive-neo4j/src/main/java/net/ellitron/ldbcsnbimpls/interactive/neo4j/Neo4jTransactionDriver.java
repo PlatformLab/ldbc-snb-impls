dea7342017e9e3ed34697ee616d1eaab28612b48
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-04-21 12:24:03 -0700

    Changed API again.
    Made the (hopefully) last modification to the API (see comments for
    details), and decided to ultimately stick with an "enqueue" mechanism,
    with an additional execAndCommit method.

--- snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionDriver.java
+++ snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionDriver.java
@@ -7,33 +7,28 @@ package net.ellitron.ldbcsnbimpls.interactive.neo4j;
 
 /**
  * A class for driving the execution of transactional Cypher statements against
- * a remote Neo4j server running a transactional Cypher HTTP endpoint. The
- * driver is designed to be used primarily in one of two ways, interactive
- * style or batch style. In interactive style the application executes one or
- * more statements and views the results before executing more statements and
- * eventually committing the transaction (or rolling it back). An example may
- * be reading a subgraph in one statement, computing page-rank, then writing
- * page rank values to the vertices in a second statement. In batch style the
- * application doesn't need to see intermediate results, and so opens a new
- * transaction, executes one or more statements, and commits the transaction in
- * a single request to the server. An example may be adding a "likes" edge in
- * the graph between a specific user and a specific post, which can be
- * expressed in a single cypher statement.
+ * a remote Neo4j server over the Neo4j REST API. In addition to supporting
+ * typical interactive style transactions where the application executes
+ * statements and views results in a loop before ultimately deciding to commit
+ * or rollback, the driver also supports "blind" transactions where the
+ * application composes one or more statements and both executes and commits in
+ * one request to the server. In certain situations (for instance, adding a
+ * node to the graph) this can save a round trip to the database and decrease
+ * overall latency.
  * <p>
  *
  * Failure Scenarios:<br>
  * Since the operation of the driver depends on communicating over the network
- * to a remote server that has state, there are a number of basic failure
- * scenarios the user should prepare for. At a high level there are three basic
- * failure scenarios:
+ * to a remote server, there are a number of basic failure scenarios the user
+ * should prepare for. At a high level there are three basic failure scenarios:
  * <ul>
  * <li>Cannot establish connection to the server</li>
  * <li>Connection established, but not getting a request response</li>
- * <li>Got a response, but it represents an error (for instance, in the event
- * of a malformed query, a timed-out transaction, or a server error)</li>
+ * <li>Got a response, but it represents an error (for instance a timed-out
+ * transaction or a server-specific error)</li>
  * </ul>
- * These failure cases are documented in more detail in the documentation for
- * the methods.
+ * These failure cases are described in more detail in the class method
+ * documentation.
  * <p>
  *
  * Special Notes:<br>
@@ -43,8 +38,8 @@ package net.ellitron.ldbcsnbimpls.interactive.neo4j;
  * <li>The driver does not currently support authentication with the server.
  * Authentication must be turned off (see Neo4j documentation).</li>
  * <li>Open transactions on the Neo4j server are automatically rolled back
- * after a timeout period. Be sure to adjust your timeout configuration value
- * to meet your application needs.</li>
+ * after a timeout period of inactivity. Be sure to adjust your timeout
+ * configuration value to meet your application needs.</li>
  * </ul>
  * <p>
  *
@@ -72,71 +67,94 @@ public class Neo4jTransactionDriver {
   }
 
   /**
-   * Execute statements in the current transaction context and return the
-   * results. If there is no currently open transaction context, a new one is
-   * started on the server. Subsequent calls will continue to execute
-   * statements in the same transaction. Result ordering in the returned array
-   * matches the statement ordering in the arguments.
+   * Add one or more statements to the driver statement queue for future
+   * execution. When executed, statements are always executed in FIFO order. A
+   * call to exec, commit, or rollback will generally have the effect of
+   * clearing the statement queue unless otherwise noted in special
+   * circumstances.
+   *
+   * @param stmts Neo4jStatement(s) to stage for later execution.
+   */
+  public void enqueue(Neo4jStatement... stmts) {
+
+  }
+
+  /**
+   * Execute enqueued statements in the current transaction context, clear the
+   * statement queue, and return the results. If there is no currently open
+   * transaction context then a new one is opened on the server automatically.
+   * Subsequent calls will then execute newly enqueued statements in the same
+   * transaction until a call to either commit or rollback is made.
    * <p>
    * Usage Notes:<br>
    * This method is used to execute statements in an interactive fashion where
    * the results can be viewed before executing more statements and eventually
-   * deciding whether to commit the transaction or roll it back. Note, however,
-   * that Neo4j transactions have a timeout value configured on the server, and
-   * if enough time passes without activity then the transaction is
-   * automatically rolled back. The timeout is reset each time statement(s) are
-   * executed.
+   * deciding whether to commit the transaction or to roll it back. Such use
+   * looks like the following:<br>
+   * <code>
+   * //... do work
+   * driver.enqueue(s1)
+   * //... do work
+   * driver.enqueue(s2, s3)
+   * results = driver.exec()
+   * // analyze results {s1: results[0], s2: results[1], s3: results[2]}
+   * // generate new statements
+   * driver.enqueue(s4, s5, s6)
+   * results = driver.exec()
+   * // analyze results
+   * // decide to commit (or rollback)
+   * driver.commit(); // or driver.rollback()
+   * </code> <br>
+   * When interactive use is not needed, {@link #execAndCommit()} can be used
+   * to execute statements and commit the transaction in one method call.
    *
-   * @param stmts Neo4jStatement(s) to execute. Statements are executed in the
-   * order they appear in the arguments.
-   *
-   * @return Array of results, where the ordering matches with the statement
-   * ordering in the arguments. That is, the ith result is for the ith
-   * statement in the argument.
+   * @return Array of results, where the ordering matches the statement enqueue
+   * ordering in the driver (that is, the nth result is for the nth enqueued
+   * statement).
    */
-  public Neo4jStatementResult[] exec(Neo4jStatement... stmts) {
+  public Neo4jStatementResult[] exec() {
 
   }
 
   /**
-   * Execute statements in the current transaction context, commit the
-   * transaction, and return the results. If there is no currently open
-   * transaction context, a new one is started on the server (and subsequently
-   * committed before returning). Result ordering in the returned array matches
-   * the statement ordering in the arguments.
+   * Same as {@link #exec()}, except that this method will commit the
+   * transaction before returning to the caller. In the case that there is no
+   * currently open transaction context, this method takes advantage of Neo4j's
+   * ability to execute a sequence of statements in a new transaction and
+   * commit the transaction all within a single request to the server.
    * <p>
    * Usage Notes:<br>
-   * This method is most commonly used to open a new transaction, execute one
-   * or more statements, and (blindly) commit the transaction all in one-shot.
-   * The Neo4j HTTP endpoint supports this combined execute and commit
-   * operation in a single request to the server, affording efficiency gains
-   * when the application does not need to see the execution results before
-   * deciding to commit. Of course, this method can be called during a running
-   * transaction as well to "top it off" with a few more statements and commit
-   * the whole transaction in one request.
+   * This method is particularly convenient and efficient for situations where
+   * the decision to commit is not dependent on statement execution results
+   * (for instance, adding a node to the graph). A typical use case might look
+   * something like this:<br>
+   * <code>
+   * while ((line = employees.readLine()) != null) {
+   *   // make cypher CREATE statement for employee
+   *   driver.enqueue(stmt);
+   * }
+   * driver.execAndCommit();
+   * </code>
    *
-   * @param stmts Neo4jStatement(s) to execute. Statements are executed in the
-   * order they appear in the arguments.
-   *
-   * @return Array of results, where the ordering matches with the statement
-   * ordering in the arguments. That is, the ith result is for the ith
-   * statement in the argument.
+   * @return Array of results, where the ordering matches the statement enqueue
+   * ordering in the driver (that is, the nth result is for the nth enqueued
+   * statement).
    */
-  public Neo4jStatementResult[] execAndCommit(Neo4jStatement... stmts) {
+  public Neo4jStatementResult[] execAndCommit() {
 
   }
 
   /**
-   * Commit the currently open transaction. If there is no open transaction
-   * then the method returns immediately.
+   * Commit the current transaction. If there is no open transaction then the
+   * method returns immediately.
    */
   public void commit() {
 
   }
 
   /**
-   * Roll back the currently open transaction. If there is no open transaction
-   * then the method returns immediately.
+   * Rollback current transaction. If there is no open transaction then the
+   * method returns immediately.
    */
   public void rollback() {
 
