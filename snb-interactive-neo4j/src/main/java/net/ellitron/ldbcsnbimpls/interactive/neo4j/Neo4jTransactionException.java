2f654237268791051cc6a1a33179a64b3514e0c1
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-04-23 22:52:38 -0700

    Added GNU GPL header for exception classes.

--- snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionException.java
+++ snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/Neo4jTransactionException.java
@@ -1,12 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2016 Stanford University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.ellitron.ldbcsnbimpls.interactive.neo4j;
 
 /**
- * A runtime exception that signals there was a problem executing a transaction
+ * A runtime exception that signals there was an error executing a transaction
  * on the Neo4j server.
  *
  * @author Jonathan Ellithorpe (jde@cs.stanford.edu)
