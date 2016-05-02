5759defd17aae0ac6ee47aeb89953d4486b806c3
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-05-02 08:46:40 -0700

    Add implementations for short queries 2-7.

--- snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/util/QueryTester.java
+++ snb-interactive-neo4j/src/main/java/net/ellitron/ldbcsnbimpls/interactive/neo4j/util/QueryTester.java
@@ -20,6 +20,13 @@ import com.ldbc.driver.DbException;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.runtime.ConcurrentErrorReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery2PersonPosts;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery3PersonFriends;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery4MessageContent;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery5MessageCreator;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery6MessageForum;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery7MessageReplies;
+import java.util.List;
 import java.util.Map;
 import net.ellitron.ldbcsnbimpls.interactive.neo4j.Neo4jDb;
 import net.ellitron.ldbcsnbimpls.interactive.neo4j.Neo4jDbConnectionState;
@@ -38,7 +45,7 @@ public class QueryTester {
       + "\n"
       + "Usage:\n"
       + "  QueryTester [--host=<host>] [--port=<port>] shortquery1 <personId>\n"
-      + "  QueryTester [--host=<host>] [--port=<port>] shortquery2 <personId>\n"
+      + "  QueryTester [--host=<host>] [--port=<port>] shortquery2 <personId> <limit>\n"
       + "  QueryTester [--host=<host>] [--port=<port>] shortquery3 <personId>\n"
       + "  QueryTester [--host=<host>] [--port=<port>] shortquery4 <messageId>\n"
       + "  QueryTester [--host=<host>] [--port=<port>] shortquery5 <messageId>\n"
@@ -81,13 +88,94 @@ public class QueryTester {
 
     if ((Boolean) opts.get("shortquery1")) {
       Long id = Long.decode((String) opts.get("<personId>"));
+
       LdbcShortQuery1PersonProfile operation =
           new LdbcShortQuery1PersonProfile(id);
 
       new Neo4jDb.LdbcShortQuery1PersonProfileHandler()
           .executeOperation(operation, dbConnectionState, resultReporter);
-      
-      System.out.println(resultReporter.result().toString());
+
+      printResult(resultReporter.result());
+    } else if ((Boolean) opts.get("shortquery2")) {
+      Long id = Long.decode((String) opts.get("<personId>"));
+      int limit = Integer.decode((String) opts.get("<limit>"));
+
+      LdbcShortQuery2PersonPosts operation =
+          new LdbcShortQuery2PersonPosts(id, limit);
+
+      new Neo4jDb.LdbcShortQuery2PersonPostsHandler()
+          .executeOperation(operation, dbConnectionState, resultReporter);
+
+      printResult(resultReporter.result());
+    } else if ((Boolean) opts.get("shortquery3")) {
+      Long id = Long.decode((String) opts.get("<personId>"));
+
+      LdbcShortQuery3PersonFriends operation =
+          new LdbcShortQuery3PersonFriends(id);
+
+      new Neo4jDb.LdbcShortQuery3PersonFriendsHandler()
+          .executeOperation(operation, dbConnectionState, resultReporter);
+
+      printResult(resultReporter.result());
+    } else if ((Boolean) opts.get("shortquery4")) {
+      Long id = Long.decode((String) opts.get("<messageId>"));
+
+      LdbcShortQuery4MessageContent operation =
+          new LdbcShortQuery4MessageContent(id);
+
+      new Neo4jDb.LdbcShortQuery4MessageContentHandler()
+          .executeOperation(operation, dbConnectionState, resultReporter);
+
+      printResult(resultReporter.result());
+    } else if ((Boolean) opts.get("shortquery5")) {
+      Long id = Long.decode((String) opts.get("<messageId>"));
+
+      LdbcShortQuery5MessageCreator operation =
+          new LdbcShortQuery5MessageCreator(id);
+
+      new Neo4jDb.LdbcShortQuery5MessageCreatorHandler()
+          .executeOperation(operation, dbConnectionState, resultReporter);
+
+      printResult(resultReporter.result());
+    } else if ((Boolean) opts.get("shortquery6")) {
+      Long id = Long.decode((String) opts.get("<messageId>"));
+
+      LdbcShortQuery6MessageForum operation =
+          new LdbcShortQuery6MessageForum(id);
+
+      new Neo4jDb.LdbcShortQuery6MessageForumHandler()
+          .executeOperation(operation, dbConnectionState, resultReporter);
+
+      printResult(resultReporter.result());
+    } else if ((Boolean) opts.get("shortquery7")) {
+      Long id = Long.decode((String) opts.get("<messageId>"));
+
+      LdbcShortQuery7MessageReplies operation =
+          new LdbcShortQuery7MessageReplies(id);
+
+      new Neo4jDb.LdbcShortQuery7MessageRepliesHandler()
+          .executeOperation(operation, dbConnectionState, resultReporter);
+
+      printResult(resultReporter.result());
+    }
+  }
+
+  /**
+   * Print a result from executing one of the queries.
+   */
+  private static void printResult(Object result) {
+    if (result == null) {
+      System.out.println("Query returned no results.");
+      return;
+    }
+    
+    if (result instanceof List) {
+      List<?> list = (List) result;
+      for (Object o : list) {
+        System.out.println(o.toString());
+      }
+    } else {
+      System.out.println(result.toString());
     }
   }
 }
