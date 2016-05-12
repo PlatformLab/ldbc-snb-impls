dbaf5d628de22997290926fe96c2560dd4f28328
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-05-12 16:42:35 -0700

    Moving IdGenerator and TorcGraphLoader to util package.

--- snb-interactive-torc/src/main/java/net/ellitron/ldbcsnbimpls/interactive/torc/util/TorcGraphLoader.java
+++ snb-interactive-torc/src/main/java/net/ellitron/ldbcsnbimpls/interactive/torc/util/TorcGraphLoader.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.ellitron.ldbcsnbimpls.interactive.torc;
+package net.ellitron.ldbcsnbimpls.interactive.torc.util;
 
 import net.ellitron.ldbcsnbimpls.snb.Entity;
 
