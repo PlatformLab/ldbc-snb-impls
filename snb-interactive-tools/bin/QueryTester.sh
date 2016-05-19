5e117bcc926a65ee6926d2c7849bcc4a0f4cb89e
Jonathan Ellithorpe <jde@cs.stanford.edu>
2016-05-18 17:14:11 -0700

    Updated the QueryTester convenience script.

--- snb-interactive-tools/bin/QueryTester.sh
+++ snb-interactive-tools/bin/QueryTester.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.tools.QueryTester" -Dexec.args="$*"
+mvn exec:java -Dlog4j.configuration="file:$PWD/src/main/resources/log4j.properties" -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.tools.QueryTester" -Dexec.args="$*"
