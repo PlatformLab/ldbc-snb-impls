b160e44f1f70eaf1eb70f9a250326b632d90c7b5
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-05-24 15:27:58 -0700

    Improved comments in GraphLoader.sh

--- snb-interactive-torc/scripts/GraphLoader.sh
+++ snb-interactive-torc/scripts/GraphLoader.sh
@@ -3,12 +3,16 @@
 #
 # Loads an LDBC SNB graph into TorcDB
 
+# Argument parameters
 dataDir=$1
 coordLoc=$2
 graphName=$3
+
+# Edit these parameters as necessary
 masters=4
 threads=1
 txSize=32
+
 mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torc.util.GraphLoader" -Dexec.args="--coordLoc $coordLoc --masters $masters --graphName $graphName --numLoaders 1 --loaderIdx 0 --numThreads $threads --txSize $txSize --reportInt 2 --reportFmt OFDT nodes $dataDir";
 mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torc.util.GraphLoader" -Dexec.args="--coordLoc $coordLoc --masters $masters --graphName $graphName --numLoaders 1 --loaderIdx 0 --numThreads $threads --txSize $txSize --reportInt 2 --reportFmt OFDT edges $dataDir";
 mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torc.util.GraphLoader" -Dexec.args="--coordLoc $coordLoc --masters $masters --graphName $graphName --numLoaders 1 --loaderIdx 0 --numThreads $threads --txSize $txSize --reportInt 2 --reportFmt OFDT props $dataDir";
