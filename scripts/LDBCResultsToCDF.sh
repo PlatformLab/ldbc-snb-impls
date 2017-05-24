ef80f06cd1ef367e6425d10e7ddcbf9c52603b0a
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-05-24 11:40:53 -0700

    Improved comments for LDBCResultsToCDF.sh

--- scripts/LDBCResultsToCDF.sh
+++ scripts/LDBCResultsToCDF.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+# ./LDBCResultsToCDF.sh /path/to/ldbc_driver/results
+#
 # Given a directory containing the results output of the LDBC Social Network
 # Benchmark, produces CDFs and PDFs of query latency.
 #
 # Notes:
 #  - Currently only generates CDFs and PDFs for update queries
-#
-# e.g.
-# ./LDBCResultsToCDF.py /path/to/ldbc_driver/results
 
 resultsDir=${1}
 binSize=100
