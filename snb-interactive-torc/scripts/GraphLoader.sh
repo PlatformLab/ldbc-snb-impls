78117a6aefabae40310d59b7c7044a2adc8d9cbd
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-05-24 13:15:20 -0700

    Added GraphLoader.sh script.
    This script is a convenience script for running the TorcDB GraphLoader
    utility for loading LDBC SNB graphs.

---
+++ snb-interactive-torc/scripts/GraphLoader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ./GraphLoader.sh /path/to/ldbc/snb/social_network basic+udp:host=192.168.1.101,port=12246 ldbcsnbsfXXXX
+#
+# Loads an LDBC SNB graph into TorcDB
+
+dataDir=$1
+coordLoc=$2
+graphName=$3
+masters=4
+threads=1
+txSize=32
+mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torc.util.GraphLoader" -Dexec.args="--coordLoc $coordLoc --masters $masters --graphName $graphName --numLoaders 1 --loaderIdx 0 --numThreads $threads --txSize $txSize --reportInt 2 --reportFmt OFDT nodes $dataDir";
+mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torc.util.GraphLoader" -Dexec.args="--coordLoc $coordLoc --masters $masters --graphName $graphName --numLoaders 1 --loaderIdx 0 --numThreads $threads --txSize $txSize --reportInt 2 --reportFmt OFDT edges $dataDir";
+mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torc.util.GraphLoader" -Dexec.args="--coordLoc $coordLoc --masters $masters --graphName $graphName --numLoaders 1 --loaderIdx 0 --numThreads $threads --txSize $txSize --reportInt 2 --reportFmt OFDT props $dataDir";
