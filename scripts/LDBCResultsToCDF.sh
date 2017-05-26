283cded9c2d152b6f29cb4dc8f766de322bd1c1e
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-05-26 11:24:43 -0700

    Added short and long read cdf generation to LDBCResultsToCDF.sh
    Previously only supported updates. Now supports analyzing short and long
    reads as well.
    
    Also put in checks for when certain queries don't exist. Previously
    would error if all queries don't exist.

--- scripts/LDBCResultsToCDF.sh
+++ scripts/LDBCResultsToCDF.sh
@@ -2,11 +2,34 @@
 resultsDir=${1}
 binSize=100
 
+# Updates
 for i in $(seq 1 8);
 do
-    cat ${resultsDir}/LDBC-results_log.csv | grep "Update"$i | cut -d '|' -f4 > ${resultsDir}/LDBC-results_log.update$i.latency.csv;
-    $(dirname $0)/cdf.py ${resultsDir}/LDBC-results_log.update$i.latency.csv
-    $(dirname $0)/pdf.py ${binSize} ${resultsDir}/LDBC-results_log.update$i.latency.csv
+    cat ${resultsDir}/LDBC-results_log.csv | grep "LdbcUpdate"$i | cut -d '|' -f4 > ${resultsDir}/LDBC-results_log.update$i.latency.csv;
+    if [ -s ${resultsDir}/LDBC-results_log.update$i.latency.csv ]
+    then
+      $(dirname $0)/cdf.py ${resultsDir}/LDBC-results_log.update$i.latency.csv
+      $(dirname $0)/pdf.py ${binSize} ${resultsDir}/LDBC-results_log.update$i.latency.csv
+    fi
 done
 
+# Short reads
+for i in $(seq 1 7);
+do
+    cat ${resultsDir}/LDBC-results_log.csv | grep "LdbcShortQuery"$i | cut -d '|' -f4 > ${resultsDir}/LDBC-results_log.shortread$i.latency.csv;
+    if [ -s ${resultsDir}/LDBC-results_log.shortread$i.latency.csv ]
+    then
+      $(dirname $0)/cdf.py ${resultsDir}/LDBC-results_log.shortread$i.latency.csv
+      $(dirname $0)/pdf.py ${binSize} ${resultsDir}/LDBC-results_log.shortread$i.latency.csv
+    fi
+done
 
+for i in $(seq 1 14);
+do
+    cat ${resultsDir}/LDBC-results_log.csv | grep "LdbcQuery"$i | cut -d '|' -f4 > ${resultsDir}/LDBC-results_log.longread$i.latency.csv;
+    if [ -s ${resultsDir}/LDBC-results_log.longread$i.latency.csv ]
+    then
+      $(dirname $0)/cdf.py ${resultsDir}/LDBC-results_log.longread$i.latency.csv
+      $(dirname $0)/pdf.py ${binSize} ${resultsDir}/LDBC-results_log.longread$i.latency.csv
+    fi
+done
